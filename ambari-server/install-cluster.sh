d4c4941eebb2c6178230810858abf10ed07d47c3
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2014-09-19 19:36:54 +0200

    run tez jar copy when cluster is ready

--- ambari-server/install-cluster.sh
+++ ambari-server/install-cluster.sh
@@ -2,16 +2,12 @@
 
 ./ambari-shell.sh << EOF
 blueprint defaults
-blueprint add --url https://gist.githubusercontent.com/keyki/1c62d179a5fef98aad92/raw/e5052566fa3aa75d325b66726933d388edf3d8cd/multinode-with-tez
-blueprint add --url https://gist.githubusercontent.com/keyki/09c6469b649caa1c66f1/raw/124dd2f337a529c825f45d0b472b03fca2634d51/singlenode-with-tez
+blueprint add --url https://gist.githubusercontent.com/keyki/1c62d179a5fef98aad92/raw/6f3ee799e13febb59387a74b810be1a0008ec75f/multinode-with-tez
+blueprint add --url https://gist.githubusercontent.com/keyki/09c6469b649caa1c66f1/raw/a750a012e9c1fb741701b2879afbee3de15c6bdb/singlenode-with-tez
 cluster build --blueprint $BLUEPRINT
 cluster autoAssign
 cluster create --exitOnFinish true
 EOF
 
-if [[ "$BLUEPRINT" =~ .*-tez  ]]; then
-  ./ambari-shell.sh << EOF
-  configuration modify --type mapred-site --key mapreduce.framework.name --value yarn-tez
-  exit
-EOF
-fi
+SERF_RPC_ADDR=${AMBARISERVER_PORT_7373_TCP##*/}
+serf event --rpc-addr=$SERF_RPC_ADDR copy
